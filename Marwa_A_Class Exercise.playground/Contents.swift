3df8bace48e607a22b16aebde6975d59edc51b4b
MarwaAbdul <86677401+MarwaAbdul@users.noreply.github.com>
2021-07-26 01:36:47 -0500

    Add files via upload

---
+++ Marwa_A_Class Exercise.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//Build A Car exercise?
+
+class JeepWrangler {
+    var carColor: String = "black"
+    var engineType: String = "3.6 LV6"
+    var seatMaterial: String = "Leather"
+    var windowType: String = "tinted and shatterproof"
+
+    var carReso = JeepWrangler()
+    
+    func dreamCar(carColor: String, enginetype: String, seatMaterial: String, windowType: String) -> String {
+        print;return "When I go car shopping, I generally look for cars with \(carColor) because I like to go fast. I'd want \(seatMaterial) and \(windowType) because they'd make me feel like the main character. lastly, while not as important as the other attributes, I'd prefer a car that is \(carColor)"
+        
+      
+}
+}
